6523c1089123daf9851bd7c1da96a97ee935fb85
Arvind Kharwal <karvind629@gmail.com>
2026-01-26 14:01:37 +0530

    26 January 2025

---
+++ Unit 01/Example_Java.java
@@ -0,0 +1,16 @@
+class Example_Java {
+    public static void main(String[] args) {
+        System.out.println("This is an example Java class.");
+    }
+}
+class Example4 {
+    public static void main(String[] args) {
+            System.out.println("This is Example4 class.");
+    }
+
+}
+class Example5 {
+    public static void main(String[] args) {
+            System.out.println("This is Example5 class.");
+    }
+}
