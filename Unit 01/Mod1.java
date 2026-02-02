2d6740ec9556540d86e5e2e2d457f4aa1ce29d2a
Arvind Kharwal <karvind629@gmail.com>
2026-02-02 10:58:24 +0530

    2 February 2026

--- Unit 01/Mod1.java
+++ Unit 01/Mod1.java
@@ -3,7 +3,9 @@ public class Mod1 {
         int a = 5;
         int b;
         // b = a++ + ++a;
-        b = ++a + a++;
+        // b = ++a + a++;
+        // b = ++a + ++a;
+        b = a++ + a++;
         System.out.println("a: "+a+"\nb: "+b);
     }
 }
