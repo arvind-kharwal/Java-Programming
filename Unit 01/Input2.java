8141b0fb96b878dbfb07ef5b32c4f2cc4d05ba37
Arvind Kharwal <karvind629@gmail.com>
2026-02-02 10:54:36 +0530

    2 February 2026

--- Unit 01/Input2.java
+++ Unit 01/Input2.java
@@ -1,3 +1,12 @@
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
 public class Input2 {
+    public static void main(String[] args) throws Exception{
+        System.out.println("Enter your number: ");
+        InputStreamReader in = new InputStreamReader(System.in);
+        BufferedReader b = new BufferedReader(in);
+        int n = Integer.parseInt(b.readLine());
+        System.out.println(n);
+    }
     
 }
---
+++ Unit 01/Mod1.java
@@ -0,0 +1,8 @@
+public class Mod1 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b;
+        b = ++a;
+        System.out.println("a: "+a+"b: "+b);
+    }
+}
