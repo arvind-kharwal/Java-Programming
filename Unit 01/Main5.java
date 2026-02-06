08c4e7861d8b5f8153dfb4f5ac50d49b05323064
Arvind Kharwal <karvind629@gmail.com>
2026-02-06 11:30:18 +0530

    6 February 2026

---
+++ Unit 01/Main5.java
@@ -0,0 +1,13 @@
+class Demo1{
+    int x;
+    String name;
+}
+public class Main5 {
+    public static void main(String[] args) {
+        Demo1 D = new Demo1();
+        D.x = 10;
+        D.name = "Arvind";
+        System.out.println(D.x);
+        System.out.println(D.name);
+    }    
+}
