8db21458222cab7f961bae069e68d4166997907b
Arvind Kharwal <karvind629@gmail.com>
2026-02-06 11:37:37 +0530

    6 February 2026

---
+++ Unit 01/Set.java
@@ -0,0 +1,19 @@
+class Demo{
+    int r;
+    String name;
+    public int getR() {
+        return r;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setR(int r) {
+        this.r = r;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+public class Set {
+    
+}
