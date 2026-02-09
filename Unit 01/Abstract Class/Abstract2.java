9415ee2e5b355e183dbc2b11b9ea556e45d1d7db
Arvind Kharwal <karvind629@gmail.com>
2026-02-09 11:44:11 +0530

    9 February 2026

---
+++ Unit 01/Abstract Class/Abstract2.java
@@ -0,0 +1,24 @@
+abstract class Calculator{
+    abstract void display();
+}
+class Add extends Calculator{
+    void display(){
+        System.out.println("Addition");
+    }
+}
+class Sub extends Calculator{
+    void display(){
+        System.out.println("Subtraction");
+    }
+}
+
+public class Abstract2 {
+    public static void main(String[] args) {
+        // Calculator C = new Calculator();
+        Add A = new Add();
+        A.display();
+        Sub B = new Sub();
+        B.display();
+    }
+    
+}
