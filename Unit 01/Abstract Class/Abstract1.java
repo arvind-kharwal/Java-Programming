10bf5e653097015a4327b98d08a077fc031ede18
Arvind Kharwal <karvind629@gmail.com>
2026-02-09 11:33:30 +0530

    9 February 2026

--- Unit 01/Abstract Class/Abstract1.java
+++ Unit 01/Abstract Class/Abstract1.java
@@ -4,9 +4,14 @@ abstract class ABS{
 abstract class XYZ extends ABS{
     // no implementation
 }
+class Child0 extends XYZ{
+    void display(){
+        System.out.println("I am in child class");
+    }
+}
 public class Abstract1 {
     public static void main(String[] args) {
-        XYZ X = new XYZ();
+        Child0 X = new Child0();
         X.display();
     }
 }
