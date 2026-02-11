bb3429c614d693fb42d4aa0b05487eac4e1dc4b0
Arvind Kharwal <karvind629@gmail.com>
2026-02-11 12:24:28 +0530

    11 February 2026

--- Unit 01/Final/Main.java
+++ Unit 01/Final/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         final int a = 10;
-        // a++;
-        StringBuffer S = new StringBuffer("Hello");
-        System.out.println(S);
-        System.out.println(a);
+        int b = a+10;
+        System.out.println(a+" "+b);
     }
     
 }
