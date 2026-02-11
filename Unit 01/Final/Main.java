4063d13cee9a32a13c0f712b6b22f4d7d1fee816
Arvind Kharwal <karvind629@gmail.com>
2026-02-11 12:25:44 +0530

    11 February 2026

--- Unit 01/Final/Main.java
+++ Unit 01/Final/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        final int a = 10;
-        int b = a+10;
-        System.out.println(a+" "+b);
+        final int a;
+        a = 10;
+        System.out.println(a);
     }
     
 }
