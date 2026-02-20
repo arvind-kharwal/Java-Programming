d4234e0334d8f2cc46ab2316cef0478035781a8f
Arvind Kharwal <karvind629@gmail.com>
2026-02-20 11:10:37 +0530

    12 February 2026

--- Unit 01/Main6.java
+++ Unit 01/Main6.java
@@ -1,3 +1,14 @@
+interface Test1{
+    void display();
+}
+class Example1 implements Test1{
+    public void display(){
+        System.out.println("Display");
+    }
+}
 public class Main6 {
-    
+    public static void main(String[] args) {
+        Example1 E1 = new Example1();
+        E1.display();
+    }
 }
