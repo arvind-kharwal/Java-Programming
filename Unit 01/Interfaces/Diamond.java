9d6805b99a6515c369ebc51bf0f879a1d5d9cff6
Arvind Kharwal <karvind629@gmail.com>
2026-02-22 21:56:47 +0530

    22 February 2026

---
+++ Unit 01/Interfaces/Diamond.java
@@ -0,0 +1,26 @@
+interface I1{
+    default void display(){
+        System.out.println("Display in Interface I1");
+    }
+}
+interface I2 extends I1{
+        default void display(){
+            System.out.println("Display in I2");
+        }
+}
+interface I3 extends I1{
+    default void display(){
+        System.out.println("Display in I3");
+    }
+}
+class DiamondExample implements I2,I3{
+    public void display(){
+        System.out.println("Display in DiamondExample");
+    }
+}
+public class Diamond {
+    public static void main(String[] args) {
+        DiamondExample D = new DiamondExample();
+        D.display();
+    }
+}
