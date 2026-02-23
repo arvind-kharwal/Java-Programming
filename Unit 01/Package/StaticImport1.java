af54fe9f1965fb7745870cd6eb0704fb21f62383
Arvind Kharwal <karvind629@gmail.com>
2026-02-23 10:12:49 +0530

    23 February 2026

---
+++ Unit 01/Package/StaticImport1.java
@@ -0,0 +1,8 @@
+import static java.lang.Math.sqrt;
+
+public class StaticImport1 {
+    public static void main(String[] args) {
+        double r = sqrt(25);
+        System.out.println(r);
+    }
+}
