d931db59bdfbf91e287f724cfdaa1466c20b65b0
Arvind Kharwal <karvind629@gmail.com>
2026-02-23 10:10:45 +0530

    23 February 2026

---
+++ Unit 01/Package/StaticImport.java
@@ -0,0 +1,9 @@
+import static java.lang.Math.PI;
+
+public class StaticImport {
+    public static void main(String[] args) {
+        int r = 2;
+        double a = PI*r*r;
+        System.out.println(a);
+    }
+}
