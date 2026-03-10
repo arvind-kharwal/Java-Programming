3ea8490bba17241efb6a2185dd09a5f4afc2e954
Arvind Kharwal <karvind629@gmail.com>
2026-03-10 10:15:54 +0530

    10 March 2026

---
+++ Unit 04/Stack/Stack1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+    
+public class Stack1{
+    public static void main(String args[]){
+    Stack<Integer> s= new Stack<Integer>();
+    System.out.println("Size at the beginning "+s.size());
+    s.push(99);
+    s.push(28);
+    s.push(17);
+    s.push(74);
+    s.push(1);
+    System.out.println("New Stack" + s);
+    System.out.println("Size after addition "+s.size());
+    System.out.println("Popped element " + s.pop());
+    System.out.println("New Stack after popping"+ s);
+    System.out.println("Size after removal "+s.size());
+    System.out.println("Top-most element " + s.peek());
+    System.out.println("Size after Peek "+s.size());
+    
+    }
+    }
