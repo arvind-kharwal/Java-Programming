775e22dc97bfa879949b2cc99f3dc97021c8c80b
Arvind Kharwal <karvind629@gmail.com>
2026-03-11 11:01:17 +0530

    11 March 2026

---
+++ Unit 04/Array List/CreateArrayList.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class CreateArrayList {
+    public static void main(String[] args) {
+    int n = 5;
+    ArrayList<Integer> arr = new ArrayList<Integer>(n);
+    System.out.println("ArrayList created successfully");
+    }
+}
