17aad1a88365cbf4ccdaab5653013eb4b2454553
Arvind Kharwal <karvind629@gmail.com>
2026-03-24 14:44:50 +0530

    24 March 2026

--- Unit 03/Functional Interface/Main2.java
+++ Unit 03/Functional Interface/Main2.java
@@ -3,6 +3,7 @@ interface Sample2{
 }
 public class Main2 {
     public static void main(String[] args) {
-        
+        Sample2 S = ()-> System.out.println("Display method using lambda expression");
+        S.display();
     }
 }
