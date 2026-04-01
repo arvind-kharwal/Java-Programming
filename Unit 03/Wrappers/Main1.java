8e2a4be33d8ab09cab41d2f7f051d272635e22c5
Arvind Kharwal <karvind629@gmail.com>
2026-04-01 11:55:00 +0530

    1 April 2026

--- Unit 03/Wrappers/Main1.java
+++ Unit 03/Wrappers/Main1.java
@@ -2,7 +2,9 @@ public class Main1 {
     public static void main(String[] args) {
         Integer I1 = 10;
         System.out.println(I1);
+        Main1 M = new Main1();
         boolean r = I1 instanceof Integer;
         System.out.println(r);
+        System.out.println(M.getClass());
     }
 }
