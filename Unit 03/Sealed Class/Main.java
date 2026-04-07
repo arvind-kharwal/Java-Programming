89a8ea104033cc059307b64f64783ff7f57672f1
Arvind Kharwal <karvind629@gmail.com>
2026-04-07 15:22:26 +0530

    7 April 2026

--- Unit 03/Sealed Class/Main.java
+++ Unit 03/Sealed Class/Main.java
@@ -1,3 +1,15 @@
+sealed class ClassA permits ClassB{
+    void displayA(){
+        System.out.println("Display A method in ClassA");
+    }
+}
+final class ClassB extends ClassA{
+    void displayB(){
+        System.out.println("Display B in class B");
+    }
+}
 public class Main {
-    
+    public static void main(String[] args) {
+        
+    }
 }
