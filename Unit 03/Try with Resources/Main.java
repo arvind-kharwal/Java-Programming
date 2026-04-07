acbc7722ba951cb3835f43b4297a5dc67eb5bffd
Arvind Kharwal <karvind629@gmail.com>
2026-04-07 15:06:33 +0530

    7 April 2026

--- Unit 03/Try with Resources/Main.java
+++ Unit 03/Try with Resources/Main.java
@@ -8,6 +8,9 @@ public class Main {
             System.out.println("Enter the number: ");
             int n = S.nextInt();
             System.out.println("The entered number is: "+n);
+            byte b[] = FIS.readAllBytes();
+            String str = new String(b);
+            System.out.println(str);
         }catch(Exception e){
             e.printStackTrace();
             System.out.println(e.getMessage());
