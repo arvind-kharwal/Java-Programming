bc69b01c0893b06b4ddf653257ce581282741f7c
Arvind Kharwal <karvind629@gmail.com>
2026-04-07 15:05:12 +0530

    7 April 2026

--- Unit 03/Try with Resources/Main.java
+++ Unit 03/Try with Resources/Main.java
@@ -1,5 +1,16 @@
+import java.io.FileInputStream;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        try(Scanner S = new Scanner(System.in);
+        FileInputStream FIS = new FileInputStream("demo.txt")){
+            System.out.println("Enter the number: ");
+            int n = S.nextInt();
+            System.out.println("The entered number is: "+n);
+        }catch(Exception e){
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
     }
 }
