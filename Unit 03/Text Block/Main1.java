25d8d034bcb04af3f8c152d31b8b01d0534c19cf
Arvind Kharwal <karvind629@gmail.com>
2026-04-07 15:14:32 +0530

    7 April 2026

--- Unit 03/Text Block/Main1.java
+++ Unit 03/Text Block/Main1.java
@@ -1,5 +1,6 @@
 public class Main1 {
     public static void main(String[] args) {
-        
+        String str = "Hello %s, I have %d legs".formatted("Arvind",2);
+        System.out.println(str);
     }
 }
