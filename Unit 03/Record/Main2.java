229e95ef4e7629423e81d2026b0060764816dcf4
Arvind Kharwal <karvind629@gmail.com>
2026-04-07 16:42:06 +0530

    7 April 2026

--- Unit 03/Record/Main2.java
+++ Unit 03/Record/Main2.java
@@ -1,9 +1,16 @@
-record Student1(int r,String name){}
+record Student1(int r,String name){
+    Student1(){
+        this(0, null);
+    }
+}
 public class Main2 {
     public static void main(String[] args) {
         Student1 S1 = new Student1(1, "Arvind");
         System.out.println(S1);
         System.out.println(S1.name());
         System.out.println(S1.r());
+        Student1 S2 = new Student1();
+        System.out.println(S2);
+
     }
 }
