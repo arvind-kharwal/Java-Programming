8fad11bfdca477acad231eaf08f7df2efadd2885
Arvind Kharwal <karvind629@gmail.com>
2026-04-07 15:49:45 +0530

    7 April 2026

--- Unit 03/forEach Method/Main1.java
+++ Unit 03/forEach Method/Main1.java
@@ -1,3 +1,10 @@
+import java.util.*;
+import java.util.function.Consumer;
 public class Main1 {
-    
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1,2,3,4,5);
+        System.out.println("The list is: "+list);
+        Consumer<Integer> C = (Integer n)-> System.out.print(n);
+        list.forEach(C);;
+    }
 }
