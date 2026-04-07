9d14c65d78ee2b1f219ad42c70557e23b701979f
Arvind Kharwal <karvind629@gmail.com>
2026-04-07 16:32:43 +0530

    7 April 2026

--- Unit 03/Record/Main.java
+++ Unit 03/Record/Main.java
@@ -1,5 +1,28 @@
+class Example{
+    private int a;
+    private int b;
+    Example(int a,int b){
+        this.a = a;
+        this.b = b;
+    }
+    public int getA() {
+        return a;
+    }
+    public int getB() {
+        return b;
+    }
+    // @Override
+    // public String toString() {
+    //     return "Example [a=" + a + ", b=" + b + "]";
+    // }
+    
+    
+}
 public class Main {
     public static void main(String[] args) {
-        
+        Example E1 = new Example(10, 20);
+        System.out.println(E1.getA());
+        System.out.println(E1.getB());
+        System.out.println(E1.getClass());
     }
 }
