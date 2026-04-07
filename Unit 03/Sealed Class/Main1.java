19abec16b25f7a8e78c3d2dfe266e882de69ae77
Arvind Kharwal <karvind629@gmail.com>
2026-04-07 15:38:27 +0530

    7 April 2026

--- Unit 03/Sealed Class/Main1.java
+++ Unit 03/Sealed Class/Main1.java
@@ -1,3 +1,32 @@
+abstract sealed class Shape permits Circle,Rectangle{
+    abstract void area();
+}
+final class Circle extends Shape{
+    private int r;
+    Circle(int r){
+        this.r = r;
+    }
+    void area(){
+        double res = Math.PI*r*r;
+        System.out.println(res);
+    }
+}
+
+final class Rectangle extends Shape{
+    private int l,b;
+    Rectangle(int l,int b){
+        this.l = l;
+        this.b = b;
+    }
+    void area(){
+        double res = l*b;
+        System.out.println(res);
+    }
+}
 public class Main1 {
-    
+    public static void main(String[] args) {
+        Circle C1 = new Circle(2);
+        C1.area();
+
+    }
 }
