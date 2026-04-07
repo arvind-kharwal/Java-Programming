ae80f2c39e687b8420b4e53095cf1c6c269f8177
Arvind Kharwal <karvind629@gmail.com>
2026-04-07 15:51:41 +0530

    7 April 2026

--- Unit 03/forEach Method/Main2.java
+++ Unit 03/forEach Method/Main2.java
@@ -1,3 +1,8 @@
+import java.util.*;
 public class Main2 {
-    
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1,2,3,4,5);
+        System.out.println("The list is: "+list);
+        list.forEach((Integer n)-> System.out.print(n));
+    }
 }
