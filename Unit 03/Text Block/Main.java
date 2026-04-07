1529cc46e0bd9a13733b02578e72cbf8a2d048bf
Arvind Kharwal <karvind629@gmail.com>
2026-04-07 15:11:39 +0530

    7 April 2026

--- Unit 03/Text Block/Main.java
+++ Unit 03/Text Block/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        
+        String str = """
+                <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Your Page Title</title>
+</head>
+<body>
+    <h1>Welcome to My Website</h1>
+    <p>This is a paragraph of text.</p>
+    <a href="https://www.example.com">This is a link</a>
+</body>
+</html>
+""";
+System.out.println(str);
+
+
     }
 }
