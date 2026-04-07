ccf403ddef6904a3f2b5645f252b8b43810cef17
Arvind Kharwal <karvind629@gmail.com>
2026-04-07 16:49:50 +0530

    7 April 2026

--- Unit 03/Record/Main3.java
+++ Unit 03/Record/Main3.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
+record Person(int r,String name){
+    public Person{
+        if(r==0)
+            throw new IllegalArgumentException();
+    }
+}
 public class Main3 {
-    
+    public static void main(String[] args) {
+    try( Scanner S = new Scanner(System.in)){
+        System.out.println("Enter r:");
+        int r1 = S.nextInt();
+        System.out.println("Enter name: ");
+        String name = S.nextLine();
+        Person P1 = new Person(r1, name);
+        System.out.println(P1);
+    }catch(IllegalArgumentException e){
+        System.out.println(e.getMessage());
+    }
+        
+    }
 }
