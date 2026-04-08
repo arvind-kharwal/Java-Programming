0613039450b417dad7516a87afa3b7fed6b4c70d
Arvind Kharwal <karvind629@gmail.com>
2026-04-08 09:26:06 +0530

    8 April 2026

--- Unit 03/Stream API/Sorted.java
+++ Unit 03/Stream API/Sorted.java
@@ -1,3 +1,12 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Sorted {
-    
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1,2,3,4,5);
+        List<Integer> res =  list.stream()
+        .sorted()
+        .collect(Collectors.toList());
+        System.out.println(res);
+}
 }
