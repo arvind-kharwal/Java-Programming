9fcb0c7d4576aaa15248c5d7bebbc7e18c76ca04
Arvind Kharwal <karvind629@gmail.com>
2026-04-08 09:18:22 +0530

    8 April 2026

---
+++ Unit 03/Stream API/Filter.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Filter {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1,2,3,4,5);
+        // Stream<Integer> strm = list.stream();
+        List<Integer> res =  list.stream()
+        .filter(n->n%2==0)
+        .collect(Collectors.toList());
+        System.out.println(res);
+    }
+}
