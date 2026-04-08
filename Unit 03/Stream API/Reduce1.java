820a650c54108d4fef4b03634d444c8f80814cfc
Arvind Kharwal <karvind629@gmail.com>
2026-04-08 10:12:52 +0530

    8 April 2026

--- Unit 03/Stream API/Reduce1.java
+++ Unit 03/Stream API/Reduce1.java
@@ -1,3 +1,10 @@
+import java.util.*;
 public class Reduce1 {
-    
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1,2,3,4,5);
+        Integer res = list.stream()
+        .reduce(2,(n,i)->n+i);
+        System.out.println(res);
+
+}
 }
