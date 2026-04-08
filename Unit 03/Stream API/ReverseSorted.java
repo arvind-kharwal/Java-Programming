8753429fe69f40a06000f099a51ecf7f458495bd
Arvind Kharwal <karvind629@gmail.com>
2026-04-08 09:48:49 +0530

    8 April 2026

--- Unit 03/Stream API/ReverseSorted.java
+++ Unit 03/Stream API/ReverseSorted.java
@@ -1,3 +1,12 @@
+import java.util.*;
+import java.util.stream.Collectors;
 public class ReverseSorted {
-    
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(11,22,3,-4,15);
+        List<Integer> res =  list.stream()
+        .sorted(Comparator.reverseOrder())
+        .collect(Collectors.toList());
+        System.out.println("Reverse order: "+res);
+}
+
 }
