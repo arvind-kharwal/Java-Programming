023908a62272af3ea5dfbd2fc2002ca4de499732
Arvind Kharwal <karvind629@gmail.com>
2026-04-08 09:20:28 +0530

    8 April 2026

--- Unit 03/Stream API/Filter1.java
+++ Unit 03/Stream API/Filter1.java
@@ -1,3 +1,13 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Filter1 {
-    
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1,2,3,4,5);
+        List<Integer> res =  list.stream()
+        .filter(n->n%2==0)
+        .map(n->n*n)
+        .collect(Collectors.toList());
+        System.out.println(res);
+    }
 }
