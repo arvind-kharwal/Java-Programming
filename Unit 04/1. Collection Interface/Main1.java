5641c849c8d65713104c22b8423b207e666d033c
Arvind Kharwal <karvind629@gmail.com>
2026-04-09 09:53:52 +0530

    9 April 2026

--- Unit 04/1. Collection Interface/Main1.java
+++ Unit 04/1. Collection Interface/Main1.java
@@ -4,12 +4,13 @@ public class Main1 {
     public static void main(String[] args) {
         Collection<Integer> numbers = new ArrayList<>();
         numbers.add(10);
-        numbers.add(20);
+        numbers.add(10);
         numbers.add(30);
         numbers.add(40);
         System.out.println(numbers);
         numbers.forEach(n -> System.out.println(n));
         numbers.remove(10);
+        System.out.println("After removal");
         numbers.forEach(n -> System.out.println(n));
     }
 }
