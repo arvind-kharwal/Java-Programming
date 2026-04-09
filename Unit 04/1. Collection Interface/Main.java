8ebe73bd420576c0707f4cdfedf6c25cdfa7fcde
Arvind Kharwal <karvind629@gmail.com>
2026-04-09 09:39:16 +0530

    9 April 2026

--- Unit 04/1. Collection Interface/Main.java
+++ Unit 04/1. Collection Interface/Main.java
@@ -1,7 +1,11 @@
+import java.util.Arrays;
 import java.util.Collection;
 
 public class Main {
     public static void main(String[] args) {
-        Collection<String> 
+        Collection<Integer> numbers = Arrays.asList(1, 2, 3);
+        numbers.add(4);
+        numbers.add(5);
+        System.out.println(numbers);
     }
 }
