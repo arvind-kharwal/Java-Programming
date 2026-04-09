7e5598a2a0b1e6730a666ad160b0f3e64ea90c6a
Arvind Kharwal <karvind629@gmail.com>
2026-04-09 12:26:19 +0530

    9 April 2026

--- Unit 04/2. List/Main3.java
+++ Unit 04/2. List/Main3.java
@@ -1,3 +1,14 @@
-public class Main3 {
+import java.util.*;
 
+public class Main3 {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(-1);
+        list.add(5);
+        System.out.println(list);
+        Collections.sort(list);
+        System.out.println(list);
+
+    }
 }
