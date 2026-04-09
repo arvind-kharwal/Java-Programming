0168e341cfbe8f6e71c6a176c9b8c29b0ea4b746
Arvind Kharwal <karvind629@gmail.com>
2026-04-09 10:00:49 +0530

    9 April 2026

--- Unit 04/1. Collection Interface/Main3.java
+++ Unit 04/1. Collection Interface/Main3.java
@@ -1,3 +1,12 @@
-public class Main3 {
+import java.util.*;
 
+public class Main3 {
+    public static void main(String[] args) {
+        Collection<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.forEach(n -> System.out.println(n));
+        boolean res = numbers.isEmpty();
+        System.out.println(res);
+    }
 }
