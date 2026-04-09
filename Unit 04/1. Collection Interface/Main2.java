66cfa0d8e7235416094b39c3dffde1df388c2891
Arvind Kharwal <karvind629@gmail.com>
2026-04-09 09:55:25 +0530

    9 April 2026

--- Unit 04/1. Collection Interface/Main2.java
+++ Unit 04/1. Collection Interface/Main2.java
@@ -1,3 +1,12 @@
-public class Main2 {
+import java.util.*;
 
+public class Main2 {
+    public static void main(String[] args) {
+        Collection<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.forEach(n -> System.out.println(n));
+        numbers.clear();
+        numbers.forEach(n -> System.out.println(n));
+    }
 }
