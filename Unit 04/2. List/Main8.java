9a320a963f782bed7024afbb6faa824b968b994e
Arvind Kharwal <karvind629@gmail.com>
2026-04-09 13:14:56 +0530

    9 April 2026

--- Unit 04/2. List/Main8.java
+++ Unit 04/2. List/Main8.java
@@ -1,3 +1,14 @@
-public class Main8 {
+import java.util.*;
 
+public class Main8 {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("C");
+        list.add("C++");
+        list.add("Python");
+        list.add("Java");
+        System.out.println(list);
+        int index = list.lastIndexOf("%");
+        System.out.println("The index is: " + index);
+    }
 }
