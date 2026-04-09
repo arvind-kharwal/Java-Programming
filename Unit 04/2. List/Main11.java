52aeb4d281d3fd5c88f3bd07266741b3dd44bc37
Arvind Kharwal <karvind629@gmail.com>
2026-04-09 14:53:04 +0530

    9 April 2026

--- Unit 04/2. List/Main11.java
+++ Unit 04/2. List/Main11.java
@@ -1,5 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main11 {
     public static void main(String[] args) {
-
+        List<String> list = new ArrayList<>();
+        list.add("C");
+        list.add("C++");
+        list.add("Python");
+        System.out.println(list);
+        boolean res = list.contains("C");
+        System.out.println(res);
     }
 }
