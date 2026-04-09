b0a208eb273284f0324460ee11c92de8158677ea
Arvind Kharwal <karvind629@gmail.com>
2026-04-09 12:04:02 +0530

    9 April 2026

--- Unit 04/2. List/Main.java
+++ Unit 04/2. List/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
         System.out.println(list);
+        list.add(10);
+        list.add(20);
+        System.out.println(list);
     }
 }
