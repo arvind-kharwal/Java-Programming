da845bc8ccf8c9a47b2f378948548b7119d4d08c
Arvind Kharwal <karvind629@gmail.com>
2026-04-09 14:24:14 +0530

    9 April 2026

--- Unit 04/2. List/Main9.java
+++ Unit 04/2. List/Main9.java
@@ -1,3 +1,12 @@
-public class Main9 {
+import java.util.*;
 
+public class Main9 {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("C");
+        list.add("C++");
+        list.add("Python");
+        list.add("C");
+        System.out.println(list.get(0));
+    }
 }
