a5d79b13663b031e4e62095de5d4eb31606ef045
Arvind Kharwal <karvind629@gmail.com>
2026-04-09 12:00:15 +0530

    9 April 2026

--- Unit 04/2. List/Main.java
+++ Unit 04/2. List/Main.java
@@ -1,3 +1,8 @@
-public class Main {
+import java.util.*;
 
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        System.out.println(list);
+    }
 }
