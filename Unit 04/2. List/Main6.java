287627b3e33c225c03b6467672f5af9509c6a26c
Arvind Kharwal <karvind629@gmail.com>
2026-04-09 12:57:52 +0530

    9 April 2026

--- Unit 04/2. List/Main6.java
+++ Unit 04/2. List/Main6.java
@@ -1,3 +1,18 @@
-public class Main6 {
+import java.util.*;
 
+public class Main6 {
+    public static void main(String[] args) {
+        List<String> list1 = new ArrayList<>();
+        list1.add("C");
+        list1.add("C++");
+        System.out.println(list1);
+        List<String> list2 = new ArrayList<>();
+        list2.add("Python");
+        list2.add("Java");
+        System.out.println(list2);
+        List<String> list3 = new ArrayList<>();
+        list3.add("Java");
+        list3.addAll(0, list1);
+        System.out.println("After Updating: " + list3);
+    }
 }
