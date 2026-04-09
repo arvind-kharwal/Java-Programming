75fcdc8ea0288af1c91bd12fe17cc5821dd83ba0
Arvind Kharwal <karvind629@gmail.com>
2026-04-09 09:46:25 +0530

    9 April 2026

--- Unit 04/1. Collection Interface/Main1.java
+++ Unit 04/1. Collection Interface/Main1.java
@@ -1,10 +1,10 @@
 import java.util.*;
 
-public class Main {
+public class Main1 {
     public static void main(String[] args) {
-        Collection<String> numbers = new ArrayList<>();
-        numbers.add("Hello");
-        numbers.add("Java");
+        Collection<Integer> numbers = new ArrayList<>();
+        numbers.add(10);
+        numbers.add(20);
         System.out.println(numbers);
         numbers.forEach(n -> System.out.println(n));
     }
