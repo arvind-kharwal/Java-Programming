b774d130da64df529f5d4207f176d1f2add1c3ad
Arvind Kharwal <karvind629@gmail.com>
2026-04-09 14:36:08 +0530

    9 April 2026

--- Unit 04/2. List/Main10.java
+++ Unit 04/2. List/Main10.java
@@ -1,3 +1,14 @@
-public class Main10 {
+import java.util.*;
 
+public class Main10 {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("C");
+        list.add("C++");
+        list.add("Python");
+        list.add("C");
+        System.out.println(list);
+        boolean b = list.isEmpty();
+        System.out.println(b);
+    }
 }
