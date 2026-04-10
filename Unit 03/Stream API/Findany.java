8068674694b6100c33875ada0c9bb6c19a802e89
Arvind Kharwal <karvind629@gmail.com>
2026-04-10 11:26:05 +0530

    10 April 2026

---
+++ Unit 03/Stream API/Findany.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Findany {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("Arvind", "Amit", "Yuvaan", "Jiya");
+        Optional<String> names = list.stream()
+                .filter(name -> name.startsWith("A"))
+                .findAny();
+        names.ifPresent(n -> System.out.println(n));
+    }
+}
