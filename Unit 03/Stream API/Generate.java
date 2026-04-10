5d13286ef44758e0577e8dd498388bd94df237d2
Arvind Kharwal <karvind629@gmail.com>
2026-04-10 11:17:54 +0530

    10 April 2026

--- Unit 03/Stream API/Generate.java
+++ Unit 03/Stream API/Generate.java
@@ -1,5 +1,12 @@
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.*;
+
 public class Generate {
     public static void main(String[] args) {
-
+        List<String> list = Stream.generate(() -> "Java")
+                .limit(10)
+                .collect(Collectors.toList());
+        System.out.println(list);
     }
 }
