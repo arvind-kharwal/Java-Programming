9a6fad6f139c696a2496b313938e7f127fcfe1a1
Arvind Kharwal <karvind629@gmail.com>
2026-04-10 11:42:30 +0530

    10 April 2026

---
+++ Unit 03/Stream API/Distinct.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Distinct {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("Arvind", "Amit", "Yuvaan", "Jiya");
+        List<String> names = list.stream()
+                .distinct()
+                .collect(Collectors.toList());
+        System.out.println(names);
+    }
+}
