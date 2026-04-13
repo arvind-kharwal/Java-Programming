cb86a15ce3e3d69595b4e97df182dacbbc855722
Arvind Kharwal <karvind629@gmail.com>
2026-04-13 10:18:14 +0530

    13 April 2026

--- Unit 04/2. List/Main14.java
+++ Unit 04/2. List/Main14.java
@@ -1,3 +1,28 @@
-public class Main14 {
+import java.util.*;
 
+class Student1 {
+    int r;
+    String name;
+
+    Student1(int r, String name) {
+        this.r = r;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student1 [r=" + r + ", name=" + name + "]";
+    }
+
+}
+
+public class Main14 {
+    public static void main(String[] args) {
+        List<Student1> list = new ArrayList<>();
+        list.add(new Student1(1, "Arvind"));
+        list.add(new Student1(30, "Yuvaan"));
+        list.add(new Student1(11, "Jiya"));
+        System.out.println("The data: ");
+        list.forEach(n -> System.out.print(n));
+    }
 }
