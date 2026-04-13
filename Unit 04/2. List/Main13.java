8c49853005a4b7859ae896bedaa0d2af04f4979b
Arvind Kharwal <karvind629@gmail.com>
2026-04-13 10:05:41 +0530

    13 April 2026

--- Unit 04/2. List/Main13.java
+++ Unit 04/2. List/Main13.java
@@ -1,3 +1,15 @@
-public class Main13 {
+class Student {
+    int r;
+    String name;
 
+    Student(int r, String name) {
+        this.r = r;
+        this.name = name;
+    }
+}
+
+public class Main13 {
+    public static void main(String[] args) {
+
+    }
 }
