2819326225548c9e8eddb2302f09b949b8556011
Arvind Kharwal <karvind629@gmail.com>
2026-04-13 11:01:55 +0530

    13 April 2026

---
+++ Unit 04/Set/Main1.java
@@ -0,0 +1,9 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main1 {
+    public static void main(String args[]) {
+        Set<String> s = new HashSet<>();
+        System.out.println("Set Elements: " + s);
+    }
+}
