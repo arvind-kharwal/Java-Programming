99222247b319be773fa5663495b737684b470496
Arvind Kharwal <karvind629@gmail.com>
2026-04-13 11:03:23 +0530

    13 April 2026

--- Unit 04/Set/Main1.java
+++ Unit 04/Set/Main1.java
@@ -3,7 +3,11 @@ import java.util.Set;
 
 public class Main1 {
     public static void main(String args[]) {
-        Set<String> s = new HashSet<>();
-        System.out.println("Set Elements: " + s);
+        Set<Integer> S = new HashSet<>();
+        S.add(10);
+        S.add(20);
+        S.add(20);
+        S.add(300);
+        System.out.println("Set Elements: " + S);
     }
 }
