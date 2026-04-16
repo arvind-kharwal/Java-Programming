3980f583cb5bf7786d22ab3250014eade2c5caf9
Arvind Kharwal <karvind629@gmail.com>
2026-04-16 14:33:21 +0530

    16 April 2026

--- Unit 04/Map/Main3.java
+++ Unit 04/Map/Main3.java
@@ -1,3 +1,14 @@
-public class Main3 {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Main3 {
+    public static void main(String[] args) {
+        Map<Integer, String> M1 = new HashMap<>();
+        M1.put(1, "A");
+        M1.put(2, "B");
+        M1.put(3, "A");
+        System.out.println(M1);
+        M1.remove(1);
+        System.out.println(M1);
+    }
 }
