f3c9199eccc632889ca704c8bfda5386c10b7ea8
Arvind Kharwal <karvind629@gmail.com>
2026-04-16 10:17:30 +0530

    16 April 2026

--- Unit 04/2. List/UserDefList.java
+++ Unit 04/2. List/UserDefList.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Student {
     int roll;
     String name;
@@ -11,5 +14,13 @@ class Student {
 }
 
 public class UserDefList {
-
+    public static void main(String[] args) {
+        List<Student> list = new ArrayList<>();
+        list.add(new Student(1, "Yuvaan", 90.34));
+        list.add(new Student(13, "Jiya", 96.30));
+        list.add(new Student(8, "Advika", 95.34));
+        for (Student s : list) {
+            System.out.println(s.roll + " " + s.name + " " + s.per);
+        }
+    }
 }
