26a9d32ea5d72fd0ace30e68524f70aff0f4e99a
Arvind Kharwal <karvind629@gmail.com>
2026-04-16 10:14:50 +0530

    16 April 2026

---
+++ Unit 04/2. List/UserDefList.java
@@ -0,0 +1,15 @@
+class Student {
+    int roll;
+    String name;
+    double per;
+
+    public Student(int roll, String name, double per) {
+        this.roll = roll;
+        this.name = name;
+        this.per = per;
+    }
+}
+
+public class UserDefList {
+
+}
