58440a2ceb8f81b86f2d06ba04a3c30d607223a8
Arvind Kharwal <karvind629@gmail.com>
2026-04-16 10:01:32 +0530

    16 April 2026

---
+++ Unit 05/cse18/src/main/java/com/abes/cse18/Example.java
@@ -0,0 +1,7 @@
+package com.abes.cse18;
+
+public class Example {
+    void display() {
+        System.out.println("Hello world");
+    }
+}
---
+++ Unit 05/cse18/src/test/java/com/abes/cse18/Cse18ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abes.cse18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cse18ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
