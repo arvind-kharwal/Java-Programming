a200d399e3d56722f48509ff62dd558ed866032d
Arvind Kharwal <karvind629@gmail.com>
2026-04-16 10:33:22 +0530

    16 April 2026

--- Unit 04/3. Comparable/Main.java
+++ Unit 04/3. Comparable/Main.java
@@ -1,3 +1,26 @@
-public class Main {
+import java.util.ArrayList;
+import java.util.List;
 
+class Student {
+    int roll;
+    String name;
+    double per;
+
+    public Student(int roll, String name, double per) {
+        this.roll = roll;
+        this.name = name;
+        this.per = per;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        List<Student> list = new ArrayList<>();
+        list.add(new Student(1, "Yuvaan", 90.34));
+        list.add(new Student(13, "Jiya", 96.30));
+        list.add(new Student(8, "Advika", 95.34));
+        for (Student s : list) {
+            System.out.println(s.roll + " " + s.name + " " + s.per);
+        }
+    }
 }
