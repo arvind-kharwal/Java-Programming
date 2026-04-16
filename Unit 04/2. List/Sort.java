a9aa208cdab9d9491f61da1313fccf369615c95f
Arvind Kharwal <karvind629@gmail.com>
2026-04-16 10:04:46 +0530

    16 April 2026

--- Unit 04/2. List/Sort.java
+++ Unit 04/2. List/Sort.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
 
 public class Sort {
     public static void main(String[] args) {
@@ -10,5 +11,7 @@ public class Sort {
         list.add(23);
         list.add(27);
         System.out.println(list);
+        Collections.sort(list);
+        System.out.println("Sorted: " + list);
     }
 }
