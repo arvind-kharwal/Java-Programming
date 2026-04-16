1851ceffe66974d0b33fa7d79edca1876bf09b2f
Arvind Kharwal <karvind629@gmail.com>
2026-04-16 10:08:16 +0530

    16 April 2026

--- Unit 04/2. List/RevSort.java
+++ Unit 04/2. List/RevSort.java
@@ -1,3 +1,20 @@
-public class RevSort {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+import java.util.Comparator;
 
+public class RevSort {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(32);
+        list.add(3);
+        list.add(2);
+        list.add(23);
+        list.add(27);
+        System.out.println("Original: " + list);
+        Collections.sort(list);
+        System.out.println("Sorted: " + list);
+        Collections.sort(list, Comparator.reverseOrder());
+        System.out.println("Reverse sorted: +list");
+    }
 }
