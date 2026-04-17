9f9c327428a8560a4c021d353ba3120f4d1c1eff
Arvind Kharwal <karvind629@gmail.com>
2026-04-17 10:50:03 +0530

    17 April 2026

--- Unit 04/2. List/List1.java
+++ Unit 04/2. List/List1.java
@@ -1,3 +1,33 @@
-public class List1 {
+import java.util.*;
 
+class Student1 implements Comparable<Student1> {
+    int r;
+    String name;
+
+    Student1(int r, String name) {
+        this.r = r;
+        this.name = name;
+    }
+
+    public int compareTo(Student1 S1) {
+        if (r == S1.r)
+            return 0;
+        else if (r > S1.r)
+            return 1;
+        else
+            return -1;
+    }
+}
+
+public class List1 {
+    public static void main(String[] args) {
+        List<Student1> list = new ArrayList<>();
+        list.add(new Student1(3, "Chandan"));
+        list.add(new Student1(7, "Dhanush"));
+        list.add(new Student1(1, "Arvind"));
+        Collections.sort(list);
+        for (Student1 i : list) {
+            System.out.println(i.r + " " + i.name);
+        }
+    }
 }
