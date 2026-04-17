294e6cad0601a86373ff7e852e4b81740f83854d
Arvind Kharwal <karvind629@gmail.com>
2026-04-17 11:09:53 +0530

    17 April 2026

--- Unit 04/2. List/List2.java
+++ Unit 04/2. List/List2.java
@@ -10,13 +10,13 @@ class Student2 {
     }
 }
 
-public class List1 {
+public class List2 {
     public static void main(String[] args) {
         List<Student2> list = new ArrayList<>();
         list.add(new Student2(3, "Chandan"));
         list.add(new Student2(7, "Dhanush"));
         list.add(new Student2(1, "Arvind"));
-        Collections.sort(list);
+        Collections.sort(list, (S1, S2) -> S1.name.compareTo(S2.r));
         for (Student2 i : list) {
             System.out.println(i.r + " " + i.name);
         }
