bb3147f9d9cddb54f4affabe6d9e64bee6220aee
Arvind Kharwal <karvind629@gmail.com>
2026-04-20 13:05:12 +0530

    20 April 2026

--- Unit 04/Array List/Main1.java
+++ Unit 04/Array List/Main1.java
@@ -1,3 +1,11 @@
-public class Main1 {
+import java.util.ArrayList;
 
+public class Main1 {
+    public static void main(String[] args) {
+        ArrayList<Object> list = new ArrayList<>();
+        list.add(10);
+        list.add(3.5);
+        list.add("Java");
+        System.out.println(list);
+    }
 }
